f4dee92e7194bc31177198b97287572fefc4f35f
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 12:39:22 +0200

    API change for getProgressSteps() function

--- dataset_wgisd_process.py
+++ dataset_wgisd_process.py
@@ -52,7 +52,7 @@ class DatasetWgisd(core.CWorkflowTask):
         else:
             self.setParam(copy.deepcopy(param))
 
-    def getProgressSteps(self, eltCount=1):
+    def getProgressSteps(self):
         # Function returning the number of progress steps for this process
         # This is handled by the main progress bar of Ikomia application
         return 3
